012cadd8fb4cec4fcc47a236a7e0704a316be033
Lucas Araujo (Home) <lucasaraujo1@ufpr.br>
2023-12-15 11:51:33 -0300

    updated all the comments in source code

--- main.cpp
+++ main.cpp
@@ -31,14 +31,14 @@ void showHelp(const std::string& programName) {
 }
 
 int main(int argc, char* argv[]) {
-    // Extrai o nome do programa a partir do caminho completo
+    // Extract the program name from the full path
     std::string programName = argv[0];
     size_t lastSlash = programName.find_last_of('/');
     if (lastSlash != std::string::npos) {
         programName = programName.substr(lastSlash + 1);
     }
 
-    // Verifica se há opção de ajuda
+    // Check for help option
     if (argc == 2) {
         std::string arg = argv[1];
         if (arg == "-h" || arg == "--help") {
@@ -47,76 +47,77 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    // Comandos que podem ser usados
+    // Commands that can be used
     const static char* COMMAND_IP{"ip a"};
 
-    /* Abrindo um pipe para o comando 'ip a'
-     * - std::unique_ptr<FILE, decltype(&pclose)>: Usa um ponteiro exclusivo para gerenciar o FILE* retornado por popen,
-     *   especificando que a função pclose deve ser usada para fechar o recurso quando o ponteiro exclusivo sai de
-     * escopo.
+    /* Open a pipe for the 'ip a' command
+     * - std::unique_ptr<FILE, decltype(&pclose)>: Uses a unique pointer to manage the FILE* returned by popen,
+     *   specifying that the pclose function should be used to close the resource when the unique pointer goes out of
+     *   scope.
      *
-     * decltype(&pclose): Retorna o tipo do ponteiro para a função pclose. No contexto do std::unique_ptr, isso informa
-     * ao compilador qual função usar para deletar o recurso quando o ponteiro exclusivo é destruído.
+     * decltype(&pclose): Returns the type of the pointer to the pclose function. In the context of std::unique_ptr, this
+     *                     informs the compiler which function to use to delete the resource when the unique pointer is
+     *                     destroyed.
      */
     std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(COMMAND_IP, "r"), pclose);
 
-    // Verifica se o pipe foi aberto corretamente
+    // Check if the pipe was opened correctly
     if (!pipe) {
-        std::cerr << "Erro ao abrir o pipe para o comando: " << COMMAND_IP << std::endl;
+        std::cerr << "Error opening pipe for command: " << COMMAND_IP << std::endl;
         return 1;
     }
 
-    // Lista para armazenar pares (nome da interface, endereço IP)
+    // List to store pairs (interface name, IP address)
     std::vector<std::pair<std::string, std::string>> interfaceList;
 
-    char buffer[128];           // Buffer para armazenar a linha lida do pipe
-    std::string interfaceName;  // Nome da interface
-    std::string ipAddress;      // Endereço IP da interface
+    char buffer[128];           // Buffer to store the line read from the pipe
+    std::string interfaceName;  // Interface name
+    std::string ipAddress;      // Interface IP address
 
-    // Lê o pipe linha por linha
+    // Read the pipe line by line
     while (fgets(buffer, sizeof(buffer), pipe.get()) != nullptr) {
         std::string line{buffer};
         std::istringstream iss{line};
 
-        // Busca uma linha que tenha o formato: #: [nome da interface]: <...
+        // Search for a line with the format: #: [interface name]: <...
         size_t pos{line.find(": <")};
         if (pos == std::string::npos) {
-            // Não encontrou o formato esperado, ignora a linha
+            // Format not found, ignore the line
             continue;
         }
 
-        // Obtém o nome da interface, ignorando o indice (3 primeiros caracteres)
+        // Get the interface name, ignoring the index (first 3 characters)
         interfaceName = line.substr(3, pos - 3);
 
-        // Busca nas linhas seguintes o endereço IP da interface
-        // Caso encontre uma linha que comece com o indice atual seguido de ":",
-        // significa que a interface não possui endereço IP configurado
-        // (pula para a próxima interface)
+        // Search the following lines for the interface IP address
+        // If a line starts with the current index followed by ":",
+        // it means the interface has no configured IP address
+        // (skip to the next interface)
         while (fgets(buffer, sizeof(buffer), pipe.get()) != nullptr) {
             line = buffer;
             pos = line.find(": <");
-            if (pos != std::string::npos) { // Encontrou uma nova interface
+            if (pos != std::string::npos) { // Found a new interface
                 /*
-                 * Salva a interface atual na lista indicando que não possui endereço IP configurado.
-                 * - std::emplace_back: Constrói diretamente um par (nome da interface, endereço IP) no final do vetor,
-                 *   evitando a necessidade de criar variáveis temporárias antes da inserção.
+                 * Save the current interface in the list indicating that it has no configured IP address.
+                 * - std::emplace_back: Directly constructs a pair (interface name, IP address) at the end of the vector,
+                 *   avoiding the need to create temporary variables before insertion.
                  */
                 interfaceList.emplace_back(interfaceName, "<no ip address>");
 
-                // Obtém o nome da nova interface, ignorando o indice (3 primeiros caracteres)
+                // Get the name of the new interface, ignoring the index (first 3 characters)
                 interfaceName = line.substr(3, pos - 3);
                 continue;
             }
 
-            // Verifica se a linha contém um endereço IP
+            // Check if the line contains an IP address
             pos = line.find("inet ");
             if (pos != std::string::npos) {
-                // Pula a string "inet " e o espaço em branco
+                // Skip the string "inet " and the space
                 pos += 5;
                 std::string ipAddress{line.substr(pos, line.find('/', pos) - pos)};
                 interfaceList.emplace_back(interfaceName, ipAddress);
 
-                // Limpa as variáveis para validar o fim do pipe
+                // Clear the variables to validate the end of the pipe
                 interfaceName.clear();
                 ipAddress.clear();
                 break;
@@ -125,30 +126,30 @@ int main(int argc, char* argv[]) {
     }
 
     if (interfaceName.length() > 0) {
-        // Salva a última interface na lista indicando que não possui endereço IP configurado
+        // Save the last interface in the list indicating that it has no configured IP address
         interfaceList.emplace_back(interfaceName, "<no ip address>");
     }
 
-    // Exibe a lista de interfaces e endereços IP
-    std::cout << "Lista de Interfaces e Endereços IP:" << std::endl;
+    // Display the list of interfaces and IP addresses
+    std::cout << "List of Interfaces and IP Addresses:" << std::endl;
     for (std::size_t i = 0; i < interfaceList.size(); ++i) {
         const auto& entry = interfaceList[i];
         std::cout << i + 1 << " - Interface: " << entry.first << ", IP: " << entry.second << std::endl;
     }
 
     std::cout << std::endl;
-    std::cout << "Escolha uma interface: ";
+    std::cout << "Choose an interface: ";
     std::size_t interfaceIndex;
     std::cin >> interfaceIndex;
-    --interfaceIndex; // Ajusta o índice para o vetor de interfaces
+    --interfaceIndex; // Adjust the index for the vector of interfaces
 
-    // Verifica se o índice da interface é válido
+    // Check if the interface index is valid
     if (interfaceIndex < 0 || interfaceIndex >= interfaceList.size()) {
-        std::cerr << "Índice de interface inválido!" << std::endl;
+        std::cerr << "Invalid interface index!" << std::endl;
         return 1;
     }
 
-    // Exibe a interface selecionada 
+    // Display the selected interface
     const auto& selectedInterface = interfaceList[interfaceIndex];
     std::cout << "IFACE=" << selectedInterface.first << std::endl;
     std::cout << "IPADDR=" << (selectedInterface.second.empty() ? "<no ip address>" : selectedInterface.second) << std::endl;
